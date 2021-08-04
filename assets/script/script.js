3b10742c865822acd2c52f6782809eb572714c09
Kevin LaCarrubba <Kevin.LaCarrubba@yahoo.com>
2021-08-04 15:49:27 -0400

    added a prompt when button is pushed.

--- assets/script/script.js
+++ assets/script/script.js
@@ -7,8 +7,11 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", (writePassword) => {
+  var passwordLenght = prompt(
+    "How many characters would you like the password to be ?, Enter a value between 8 and 128."
+  );
+});
