e91d71e5beaf2967b0ba8bc828f487a553f338e2
Kevin LaCarrubba <Kevin.LaCarrubba@yahoo.com>
2021-08-05 16:49:49 -0400

    added a function to randomize an array

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,14 +1,147 @@
-// Assignment Code
-var generateBtn = document.querySelector("#generate");
+// Array of upperCase characters
+var upperCase = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+];
+// Array of lowerCase characters
+var lowerCase = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+];
+// Array of numerical characters
+var numericalCharacters = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+// Array of special characters
+var specialCharacters = [
+  "+",
+  "-",
+  "&",
+  "|",
+  "!",
+  "(",
+  ")",
+  "{",
+  "}",
+  "[",
+  "]",
+  "^",
+  "~",
+  "*",
+  "?",
+  ":",
+  "#",
+];
+//Function to prompt user for password options.
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+function getUserOptions() {
+  var passwordLength = prompt(
+    "How many characters would you like the password to be ?, Enter a value between 8 and 128."
+  );
+  //Set user input to integer. Run conditional statement to make sure it is an actual number. Run conditional statement to make sure it is larger than 8 and less tham 128.
+  passwordLength = parseInt(passwordLength);
+  if (isNaN(passwordLength)) {
+    alert("You can only enter the length in numbers.");
+    return writePassword();
+  } else {
+    if (passwordLength < 8 || passwordLength > 128) {
+      alert("You did not enter a correct value.");
+      return writePassword();
+    }
+  }
+  console.log(passwordLength);
+  var generateBtn = document.querySelector("#generate");
 
-  passwordText.value = password;
-
+  // Write password to the #password input
+  function writePassword() {
+    var password = generatePassword();
+    var passwordText = document.querySelector("#password");
+    passwordText.value = password;
+  }
+  //Create an object to store userinput
+  var userOptions = {
+    passwordLength: passwordLength,
+    lowerCaseChar: [],
+    upperCaseChar: [],
+    specialChar: [],
+    numericalChar: [],
+  };
+  //Prompt to ask user if they would like to add lowercase characters.
+  if (
+    confirm("Would you like to add lowercase characters in your password ?")
+  ) {
+    userOptions.lowerCaseChar = userOptions.lowerCaseChar.concat(lowerCase);
+  }
+  //Promt to ask user if they would like to add uppercase characters.
+  if (
+    confirm("Would you like to add uppercase characters in your password ?")
+  ) {
+    userOptions.upperCaseChar = userOptions.upperCaseChar.concat(upperCase);
+  }
+  //Prompt user to ask if they would like numbers in their password.
+  if (confirm("Would you like to add numbers into your password ?")) {
+    userOptions.numericalChar =
+      userOptions.numericalChar.concat(numericalCharacters);
+  }
+  //Prompt user to ask if they would like special characters in their password.
+  if (confirm("Would you like to add special characters in your password ?")) {
+    userOptions.specialChar =
+      userOptions.numericalChar.concat(specialCharacters);
+  }
+  return userOptions;
 }
-
+//Function to randomize an array
+function randomizeArray(randomArray, pwLength) {
+  var generatedArray = randomArray
+    .sort(() => Math.random() - Math.random())
+    .slice(0, pwLength);
+  return generatedArray;
+}
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
